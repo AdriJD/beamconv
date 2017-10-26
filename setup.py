5871c34336181135ff8ce5827e207f30720869e4
Jon E Gudmundsson <jegudmunds@gmail.com>
2017-10-26 23:19:41 +0200

    Adding a setup.py file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+setup(name='cmb_beams',
+      version='0.1',
+      description='Code for beam convolution.',
+      url='https://github.com/oskarkleincentre/cmb_beams',
+      author='Adri J. Duivenvoorden',
+      author_email='adri.j.duivenvoorden@gmail.com',
+      license='MIT',
+      packages=['cmb_beams', 'tests'],
+      package_dir={'cmb_beams': 'python'},
+      install_requires=['healpy', 'numpy', 'qpoint'],
+      zip_safe=False)
