a8518d1e4f12ff24e3868f39eb6977f2b7056b84
Jon E. Gudmundsson <jegudmunds@gmail.com>
2017-12-19 09:44:23 +0100

    improving __init__

--- cmb_beams/__init__.py
+++ cmb_beams/__init__.py
@@ -1,2 +1,10 @@
+'''
+This package contains tools to simulate a CMB experiment that convolves the sky 
+with an asymmetric 4-pi beam.
+'''
 
+from .instrument import MPIBase, Instrument, ScanStrategy
+from .detector import Beam
+from .tools import *
+from .plot_tools import *
 
