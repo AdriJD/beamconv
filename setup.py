1a5e2040d79c6ed53dee7d4fea58f0d31178ffcd
AdriJD <adri.j.duivenvoorden@gmail.com>
2018-01-18 16:14:39 +0100

    updated directory name and setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-setup(name='cmb_beams',
+setup(name='beamconv',
       version='0.1',
       description='Code for beam convolution.',
       url='https://github.com/oskarkleincentre/cmb_beams',
       author='Adri J. Duivenvoorden',
       author_email='adri.j.duivenvoorden@gmail.com',
       license='MIT',
-      packages=['cmb_beams', 'tests'],
+      packages=['beamconv', 'tests'],
       install_requires=['healpy', 'numpy', 'qpoint'],
       test_suite='tests',
       zip_safe=False)
