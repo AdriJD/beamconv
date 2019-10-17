c79fe18e44f7ebaf677e8c00eaa2eaf7615c4b05
mbilli <52699575+matteobilli@users.noreply.github.com>
2019-10-17 12:39:54 +0200

    Add files via upload

--- beamconv/coupling_mueller_matrix.py
+++ beamconv/coupling_mueller_matrix.py
@@ -42,23 +42,83 @@ hwp4 = tm.Stack( thicknesses4, materials4, angles4)
 
 ####################################################################################################################################
 
+'''
+## ALTERNATIVE WAY TO COMPUTE THE TOTAL MUELLER MATRIX THROUGH THE MATRIX PRODUCT
+def rot_matrix(angle): 
+	c = np.cos(2*angle)
+	s = np.sin(2*angle)
+	return np.array([[1,0,0,0],[0,c,s,0],[0,-s,c,0],[0,0,0,1]])
+
+def TopRowMuellerMatrix(hwp, alph, thet, freqq, xi, psi):
+	eta = 1. ## co-polar quantity (FREEZE) 
+	delta = 0. ## cross-polar quantity (FREEZE) 
+	
+	a = (eta**2+delta**2)
+	b = (eta**2-delta**2)
+	c = 2*eta*delta
+
+	M_pol = 0.5*np.array([[a,b,0,0],[b,a,0,0],[0,0,c,0],[0,0,0,c]])
+
+	#########################
+	# REAL HWP
+	# Mueller( stack, frequency, incidenceAngle, rotation, inputIndex=1.0, exitIndex=1.0, reflected=False): 
+	# Mueller = tm.Mueller(hwp, freqq, alph, 0, reflected=False)
+	# T = Mueller[0,0]
+	# rho= Mueller[0,1]
+	# c =  Mueller[2,2]
+	# s =  Mueller[3,2]
+	#########################
+
+	
+	########################
+	# # IDEAL HWP
+	T = 1.
+	rho= 0.
+	c =  -1.
+	s =  0.
+	Mueller = np.array([[T,rho,0,0],[rho,T,0,0],[0,0,c,-s],[0,0,s,c]])
+	########################
+
+	
+	M_thet=rot_matrix(thet)
+	M_mthet=rot_matrix(-thet)
+	M_xi= rot_matrix(-xi)
+	M_psi= rot_matrix(psi)
+	M_hwp = dot(Mueller,M_thet)
+	M_hwp = dot(M_mthet,M_hwp)
+
+	M_tot = dot(M_pol, dot(M_xi, dot(M_hwp,M_psi)))
+
+	Mtr= array([M_tot[0,0],M_tot[0,1],M_tot[0,2],M_tot[0,3]])
+	return Mtr
+
+'''
+
+
 def TopRowMuellerMatrix(hwp, alph, thet, freqq, xi, psi):
 	eta = 1. ## co-polar quantity (FREEZE) 
 	delta = 0. ## cross-polar quantity (FREEZE) 
 	gamma = (eta**2-delta**2)/(eta**2+delta**2) ## polarization efficienty (FREEZE)
 	H = 0.5*(eta**2+delta**2)
-	## Ideal case: 
-	# eta = 1.
-	# delta = 0. 
-	# gamma = 1.
-	# H = 1.
-	
-	## Mueller( stack, frequency, incidenceAngle, rotation, inputIndex=1.0, exitIndex=1.0, reflected=False): 
-	Mueller = tm.Mueller(hwp, freqq, alph, 0., reflected=False)
-	T = Mueller[0,0]
-	rho= Mueller[0,1]/ Mueller[0,0]
-	c =  Mueller[2,2]/ Mueller[0,0]
-	s =  Mueller[3,2]/ Mueller[0,0]
+	## Ideal case: H = 0.5
+
+	#########################
+	## REAL HWP
+	# Mueller( stack, frequency, incidenceAngle, rotation, inputIndex=1.0, exitIndex=1.0, reflected=False): 
+	# Mueller = tm.Mueller(hwp, freqq, alph, 0., reflected=False)
+	# T = Mueller[0,0]
+	# rho= Mueller[0,1]/ Mueller[0,0]
+	# c =  Mueller[2,2]/ Mueller[0,0]
+	# s =  Mueller[3,2]/ Mueller[0,0]
+	#########################
+
+	#########################
+	# IDEAL HWP 
+	T = 1.
+	c = -1.
+	rho = 0.
+	s = 0.
+	#########################
 
 	MII = H*T*(1+(gamma*rho*np.cos((2*thet)+(2*xi))))
 	MIQ = H*T*(rho*np.cos((2*thet)+(2*psi)) + (0.5*(1+c)*gamma*np.cos((2*psi)-(2*xi))) + (0.5*(1-c)*gamma*np.cos((4*thet)+(2*xi)+(2*psi))))
@@ -66,45 +126,38 @@ def TopRowMuellerMatrix(hwp, alph, thet, freqq, xi, psi):
 	MIV = H*T*(s*gamma*np.sin((2*thet)+(2*xi)))
 	Mtr= np.array([MII,MIQ,MIU,MIV])
 	return Mtr
-	
 
+
+### ONLY HWP_ANGLE CAN BE AN ARRAY
 def coupling_system(hwp, freq, theta, alpha, xi, psi):
-	if np.isscalar (alpha) and np.isscalar (theta) and np.isscalar (freq):
+	if np.isscalar (theta):
 		M_tr = TopRowMuellerMatrix(hwp, alpha, theta, freq, xi, psi)
+		### IQUV base
 		MII = M_tr[0]
+		MIQ = M_tr[1] 
+		MIU = M_tr[2]
+
+		### IPPV base
 		MIP = 0.5*(M_tr[1]-1j*M_tr[2]) 
 		MIP_t = 0.5*(M_tr[1]+1j*M_tr[2]) 
 	else:
-		if np.isscalar (alpha) and np.isscalar (theta):
-			M_tr = np.zeros((4,len(freq)))
-			for k in range (len(freq)):
-				M_tr[:,k]= TopRowMuellerMatrix(hwp, alpha, theta, freq[k], xi, psi)
-			MII = M_tr[0,:]
-			MIP = 0.5*(M_tr[1,:]-1j*M_tr[2,:]) 
-			MIP_t = 0.5*(M_tr[1,:]+1j*M_tr[2,:]) 
-		else: 
-			if np.isscalar (alpha):
-				M_tr = np.zeros((4,len(theta),len(freq)))
-				for j in range(len(theta)):
-					for k in range (len(freq)):
-						M_tr[:,j,k]= TopRowMuellerMatrix(hwp, alpha, theta[j], freq[k], xi, psi)
-				MII = M_tr[0,:,:]
-				MIP = 0.5*(M_tr[1,:,:]-1j*M_tr[2,:,:]) 
-				MIP_t = 0.5*(M_tr[1,:,:]+1j*M_tr[2,:,:]) 
-			else:
-				M_tr =np.zeros((4,len(alpha),len(theta),len(freq)))
-				for i in range (len(alpha)):
-					for j in range(len(theta)):
-						for k in range (len(freq)):
-							M_tr[:,i,j,k]= TopRowMuellerMatrix(hwp, alpha[i], theta[j], freq[k], xi, psi)
-				MII = M_tr[0,:,:,:]
-				MIP = 0.5*(M_tr[1,:,:,:]-1j*M_tr[2,:,:,:]) 
-				MIP_t = 0.5*(M_tr[1,:,:,:]+1j*M_tr[2,:,:,:]) 
-				
+		M_tr = np.zeros((4,len(theta)))
+		for i in range (len(theta)):
+			M_tr[:,i]= TopRowMuellerMatrix(hwp, alpha, theta[i], freq, xi, psi)
+		### IQUV base
+		MII = M_tr[0,:]
+		MIQ = M_tr[1,:] 
+		MIU = M_tr[2,:]
+
+		### IPPV base
+		MIP = 0.5*(M_tr[1,:]-1j*M_tr[2,:]) 
+		MIP_t = 0.5*(M_tr[1,:]+1j*M_tr[2,:])
+
+	#return MII, MIQ, MIU 
 	return MII, MIP, MIP_t 
 
 ####################################################################################################################################
-## SETTING TEST PARAMETERS (all the angles in degrees)
+# SETTING TEST PARAMETERS (all the angles in degrees)
 
 # GHz = 1e9 
 # deg = pi/180. #from deg2rad
@@ -112,14 +165,9 @@ def coupling_system(hwp, freq, theta, alpha, xi, psi):
 # freq = linspace(10,300,1000) * GHz ## frequency range
 # theta = array([0.,2.,4.,6.,8.,10.])*deg ## HWP angle
 # alpha = array([0.0, 2.39, 4.75,  7.06, 9.29, 11.43, 13.48])*deg ## angle of incidence
-# #alpha = array([0.])*deg
 
-# eta = 1. ## co-polar quantity (FREEZE) 
-# delta = 0. ## cross-polar quantity (FREEZE) 
-# gamma = (eta**2-delta**2)/(eta**2+delta**2) ## polarization efficienty (FREEZE)
-# ## Ideal case: eta = 1., delta = 0., gamma = 1.
+# xi = 0.*deg ## angle of the detector (FREEZE)  what in sasha's note is called xi
+# psi = 10.*deg ## instrument rotation (FREEZE) 
 
-# xi = 0.*deg ## angle of the detector (FREEZE)  
-# psi = 0.*deg ## instrument rotation (FREEZE) 
 
-#coupling_system(hwp4, freq, theta, alpha, xi, psi)
+# M_II,M_IP, M_IP_t = coupling_system(hwp4, freq, theta, alpha, xi, psi)
