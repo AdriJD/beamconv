66747db5e2fe8236335cc8f94d9755df9694e81b
mbilli <52699575+matteobilli@users.noreply.github.com>
2019-12-03 14:17:55 +0100

    mini-bugs in new stacks

--- beamconv/detector.py
+++ beamconv/detector.py
@@ -52,6 +52,7 @@ class HWP(object):
         duroid_a = tm.material( 1.55, 1.55, 0.5e-4, 0.5e-4, 'RT Duroid', materialType='isotropic')
         duroid_b = tm.material( 1.715, 1.715, 1.2e-3, 1.2e-3, 'RT Duroid', materialType='isotropic')
         duroid_c = tm.material( 2.52, 2.52, 56.6e-4, 56.5e-4, 'RT Duroid', materialType='isotropic')
+        duroid_d = tm.material( 2.75, 2.75, 56.6e-4, 56.5e-4, 'RT Duroid', materialType='isotropic')
         quartz = tm.material( 1.951, 1.951, 1.2e-3, 1.2e-3, 'Quartz', materialType='isotropic')
         circlex = tm.material(1.935, 1.935, 1.2e-3, 1.2e-3, 'Circlex', materialType='isotropic')
         hdpe = tm.material(1.51, 1.51, 1e-3, 1e-3, 'HDPE', materialType='isotropic' )
@@ -75,12 +76,12 @@ class HWP(object):
         elif (model_name == '3layer_HWP'):
             thicknesses = [0.38*tm.mm, 0.32*tm.mm, 0.27*tm.mm, 3.75*tm.mm, 0.27*tm.mm, 0.32*tm.mm, 0.38*tm.mm]
             materials = [duroid_a, duroid_b, duroid_c, sapphire, duroid_c, duroid_b, duroid_a]
-            angles = [0.0, 0.0, 0.0, 0.0, 0.0]
+            angles = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
         elif (model_name == '4layer_HWP'):
             thicknesses = [0.38*tm.mm, 0.32*tm.mm, 0.27*tm.mm, 0.2*tm.mm, 3.75*tm.mm, 0.2*tm.mm, 0.27*tm.mm, 0.32*tm.mm, 0.38*tm.mm]
             materials = [duroid_a, duroid_b, duroid_c, duroid_d, sapphire, duroid_d, duroid_c, duroid_b, duroid_a]
-            angles = [0.0, 0.0, 0.0, 0.0, 0.0]
+            angles = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
 
         elif (model_name=='SPIDER_95'):
             
