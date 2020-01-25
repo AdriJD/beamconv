fdf1b8d3db25b8fc3f8c7e6a525b7da3e7093f75
AERAdler <alexandre.adler@mapadler.ch>
2020-01-25 17:23:46 +0100

    More standard HWP penalty computation

--- scratch/AHWP_optim.py
+++ scratch/AHWP_optim.py
@@ -13,7 +13,7 @@ nu_1 = (95**2+150**2)/(95+150)
 epsilon_1 = 165 /nu_1 - 1.
 delta_1 = np.pi/3 - (epsilon_1*np.pi/2)**2/(2*np.sqrt(3))
 
-nu = np.arange(80, 170, 5)
+nu = np.array((80, 85,90,95,100,105,110,135,140,145,150,155,160,165))
 theta = np.linspace(0,180,1801)
 alpha = np.linspace(0, 20, 21)
 Jones_gamma = np.zeros((nu.size,2))
@@ -25,7 +25,7 @@ s_l =[2.3e-4, 1.25e-4]
 s_t = 3.65
 
 ideal_muell_mat = np.array(((1,0,0,0),(0,1,0,0),(0,0,-1,0),(0,0,0,-1)))
-
+penalty = np.zeros(nu.size)
 A = np.zeros((nu.size,3,theta.size), dtype=complex)
 B = np.zeros((nu.size, 4,4))
 for i, freq in enumerate(nu):
@@ -33,12 +33,14 @@ for i, freq in enumerate(nu):
     dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=0.0)
     dumbeam.set_hwp_mueller(model_name='1AR3BRcent')
     A[i,:,:] = dumbeam.get_mueller_top_row_full(psi=np.zeros(theta.size), xi=np.zeros(theta.size), theta=np.radians(theta))
-    phase = np.radians(theta[np.argmax(np.real(A[i,1,:900]), axis=0)])
+    phase = np.radians(57.1)#np.radians(theta[np.argmax(np.real(A[i,1,:900]), axis=0)])
     phase_rot = np.array(((1.,0.,0.,0.), (0.,np.cos(-4*phase),np.sin(-4*phase),0.),
                         (0.,-np.sin(-4*phase),np.cos(-4*phase),0.),(0.,0.,0.,1)))#Rotateback by twice the phase
     B[i]=np.dot(phase_rot,dumbeam.hwp_mueller)
+    penalty[i] = np.sum(np.square(B[i]-ideal_muell_mat))
     print('HWP Mueller is:\n', dumbeam.hwp_mueller)
     print('Phase is:\n',np.degrees(phase))
     print('Rotated Mueller is:\n', np.dot(phase_rot,dumbeam.hwp_mueller))
-    print('Penalty is: \n',np.sum(np.square(B[i]-ideal_muell_mat)))#Sum over all elements of squared differences
+    print('Penalty is: \n',penalty[i])#Sum over all elements of squared differences
 
+print('Total_penalty:\n', np.sum(penalty))
