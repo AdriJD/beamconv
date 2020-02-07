41ae5b4221598af7018e7cfda0fdab018ac1fa8d
AERAdler <alexandre.adler@mapadler.ch>
2020-02-07 16:41:35 +0100

    New configurations based on the same solving routine

--- scratch/AHWP_optim.py
+++ scratch/AHWP_optim.py
@@ -6,6 +6,127 @@ from beamconv import Beam
 from beamconv import transfer_matrix as tm
 from scipy.optimize import minimize
 
+def penalty_monoref(x):
+    n1=x[0]; n2=x[1]; n3=x[2]; d1=x[3]; d2=x[4]; d3=x[5];
+    sp_index = np.array([3.019, 3.336])
+    sp_thick = x[6];
+    angles = np.array([0.,0.,0.,0., 0.,0.,0.])*np.pi/180.0
+    losses = np.array([ [1e-3,1e-3],[1e-3,1e-3], [1e-3,1e-3], 
+                    [2.3e-4, 1.25e-4],[1e-3,1e-3], [1e-3,1e-3], [1e-3,1e-3]])
+    thicknesses=np.array([d1, d2, d3 , sp_thick, d3, d2, d1])
+    indices=np.array([[n1, n1],[n2, n2],[n3, n3], 
+                    sp_index,[n3,n3],[n2,n2], [n1,n1]])
+
+    nu = np.array((80,85,90,95,100,105,110,135,140,145,150,155,160,165))
+
+    penalty = np.zeros(nu.size)
+    ideal_muell_mat = np.array(((1,0,0,0),(0,1,0,0),(0,0,-1,0),(0,0,0,-1)))
+
+    for i, freq in enumerate(nu):
+
+        dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=0.0)
+        dumbeam.set_hwp_mueller(thicknesses=thicknesses, 
+                        indices=indices, 
+                        losses=losses, angles=angles)
+        penalty[i] = np.sum(np.square(dumbeam.hwp_mueller-ideal_muell_mat))
+
+        dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=10.0)
+        dumbeam.set_hwp_mueller(thicknesses=thicknesses, 
+                        indices=indices, 
+                        losses=losses, angles=angles)
+        penalty[i] += np.sum(np.square(dumbeam.hwp_mueller-ideal_muell_mat))
+    return np.sum(penalty)
+
+def grad_penalty_monoref(x):
+
+    grad = np.zeros(x.size)
+    for i in range(grad.size):
+        xp=np.copy(x)
+        xm=np.copy(x)
+        xp[i]+=1e-6
+        xm[i]-=1e-6
+        grad[i]=(penalty_monoref(xp)-penalty_monoref(xm))/2e-6
+
+    return grad
+
+def penalty_mono2(x):
+    n1=x[0]; n2=x[1]; d1=x[2]; d2=x[3]; sp_thick=x[4];
+    sp_index = np.array([3.019, 3.336])
+    angles = np.array([0.,0.,0., 0.,0.])*np.pi/180.0
+    losses = np.array([ [1e-3,1e-3],[1e-3,1e-3], 
+                    [2.3e-4, 1.25e-4], [1e-3,1e-3], [1e-3,1e-3]])
+    thicknesses=np.array([d1, d2, sp_thick, d2, d1])
+    indices=np.array([[n1, n1],[n2, n2], 
+                    sp_index,[n2,n2], [n1,n1]])
+    nu = np.array((80,85,90,95,100,105,110,135,140,145,150,155,160,165))
+    penalty = np.zeros(nu.size)
+    ideal_muell_mat = np.array(((1,0,0,0),(0,1,0,0),(0,0,-1,0),(0,0,0,-1)))
+    
+    for i, freq in enumerate(nu):
+
+        dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=0.0)
+        dumbeam.set_hwp_mueller(thicknesses=thicknesses, 
+                        indices=indices, 
+                        losses=losses, angles=angles)
+        penalty[i] = np.sum(np.square(dumbeam.hwp_mueller-ideal_muell_mat))
+
+        dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=10.0)
+        dumbeam.set_hwp_mueller(thicknesses=thicknesses, 
+                        indices=indices, 
+                        losses=losses, angles=angles)
+        penalty[i] += np.sum(np.square(dumbeam.hwp_mueller-ideal_muell_mat))
+    return np.sum(penalty)
+
+def grad_penalty_mono2(x):
+
+    grad = np.zeros(x.size)
+    for i in range(grad.size):
+        xp=np.copy(x)
+        xm=np.copy(x)
+        xp[i]+=1e-6
+        xm[i]-=1e-6
+        grad[i]=(penalty_mono2(xp)-penalty_mono2(xm))/2e-6
+
+    return grad
+
+def penalty_mono1(x):
+    n1=x[0]; d1=x[1]; sp_thick=x[2];
+    sp_index = np.array([3.019, 3.336])
+    angles = np.array([0.,0.,0.])*np.pi/180.0
+    losses = np.array([ [1e-3,1e-3], [2.3e-4, 1.25e-4], [1e-3,1e-3]])
+    thicknesses=np.array([d1, sp_thick, d1])
+    indices=np.array([[n1, n1], sp_index, [n1,n1]])
+    nu = np.array((80,85,90,95,100,105,110,135,140,145,150,155,160,165))
+    penalty = np.zeros(nu.size)
+    ideal_muell_mat = np.array(((1,0,0,0),(0,1,0,0),(0,0,-1,0),(0,0,0,-1)))
+    
+    for i, freq in enumerate(nu):
+
+        dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=0.0)
+        dumbeam.set_hwp_mueller(thicknesses=thicknesses, 
+                        indices=indices, 
+                        losses=losses, angles=angles)
+        penalty[i] = np.sum(np.square(dumbeam.hwp_mueller-ideal_muell_mat))
+
+        dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=10.0)
+        dumbeam.set_hwp_mueller(thicknesses=thicknesses, 
+                        indices=indices, 
+                        losses=losses, angles=angles)
+        penalty[i] += np.sum(np.square(dumbeam.hwp_mueller-ideal_muell_mat))
+    return np.sum(penalty)
+
+def grad_penalty_mono1(x):
+
+    grad = np.zeros(x.size)
+    for i in range(grad.size):
+        xp=np.copy(x)
+        xm=np.copy(x)
+        xp[i]+=1e-6
+        xm[i]-=1e-6
+        grad[i]=(penalty_mono1(xp)-penalty_mono1(xm))/2e-6
+
+    return grad
+
 
 def penalty1(x):
     n1=x[0]; d1=x[1];
@@ -47,8 +168,8 @@ def penalty1(x):
 def grad_penalty1(x):
     n1=x[0]; d1=x[1];
     grad = np.zeros(2)
-    grad[0] = (penalty1([n1+0.0001, d1])-penalty1([n1-0.0001, d1]))/0.0002
-    grad[1] = (penalty1([n1, d1+0.0001])-penalty1([n1, d1-0.0001]))/0.0002
+    grad[0] = (penalty1([n1+1e-6, d1])-penalty1([n1-1e-6, d1]))/2e-6
+    grad[1] = (penalty1([n1, d1+1e-6])-penalty1([n1, d1-1e-6]))/2e-6
     return grad
 
 
@@ -90,12 +211,13 @@ def penalty2(x):
     return np.sum(penalty)
 
 def grad_penalty2(x):
-    n1=x[0]; n2=x[1]; d1=x[2]; d2=x[3]; 
-    grad = np.zeros(4)
-    grad[0] = (penalty2([n1+0.0001,n2, d1, d2])-penalty2([n1-0.0001, n2, d1,d2]))/0.0002
-    grad[1] = (penalty2([n1,n2+0.0001, d1, d2])-penalty2([n1,n2-0.0001, d1, d2]))/0.0002
-    grad[2] = (penalty2([n1,n2, d1+0.0001, d2])-penalty2([n1,n2, d1-0.0001, d2]))/0.0002
-    grad[3] = (penalty2([n1,n2, d1, d2+0.0001])-penalty2([n1,n2, d1,d2-0.0001 ]))/0.0002
+    grad = np.zeros(x.size)
+    for i in range(x.size):
+        xp=np.copy(x)
+        xm=np.copy(x)
+        xp[i]+=1e-6
+        xm[i]-=1e-6
+        grad[i]=(penalty2(xp)-penalty2(xm))/2e-6
     return grad
 
 
@@ -137,26 +259,26 @@ def penalty3(x):
     return np.sum(penalty)
 
 def grad_penalty3(x):
-    n1=x[0]; n2=x[1]; n3=x[2]; d1=x[3]; d2=x[4]; d3=x[5];
-    grad = np.zeros(6)
-    grad[0] = (penalty3([n1+0.0001,n2, n3, d1, d2, d3])-penalty3([n1-0.0001,n2, n3, d1, d2, d3]))/0.0002
-    grad[1] = (penalty3([n1,n2+0.0001, n3, d1, d2, d3])-penalty3([n1,n2-0.0001, n3, d1, d2, d3]))/0.0002
-    grad[2] = (penalty3([n1,n2, n3+0.0001, d1, d2, d3])-penalty3([n1,n2, n3-0.0001, d1, d2, d3]))/0.0002
-    grad[3] = (penalty3([n1,n2, n3, d1+0.0001, d2, d3])-penalty3([n1,n2, n3, d1-0.0001, d2, d3]))/0.0002
-    grad[4] = (penalty3([n1,n2, n3, d1, d2+0.0001, d3])-penalty3([n1,n2, n3, d1, d2-0.0001, d3]))/0.0002
-    grad[5] = (penalty3([n1,n2, n3, d1, d2, d3+0.0001])-penalty3([n1,n2, n3, d1, d2, d3-0.0001]))/0.0002
+    grad = np.zeros(x.size)
+    for i in range(grad.size):
+        xp=np.copy(x)
+        xm=np.copy(x)
+        xp[i]+=1e-6
+        xm[i]-=1e-6
+        grad[i]=(penalty3(xp)-penalty3(xm))/2e-6
     return grad
+
 
 def penalty5(x):
     n1=x[0]; n2=x[1]; n3=x[2]; d1=x[3]; d2=x[4]; d3=x[5];
     sp_index = np.array([3.019, 3.336])
     sp_thick = 3.86
-    angles = np.array([0.,0.,0., 0.,29.,94.5,29.,0., 0.,0.,0.])*np.pi/180.0
+    angles = np.array([0.,0.,0., 0.,29.,94.5,29.,2., 0.,0.,0.])*np.pi/180.0
     losses = np.array([ [1e-3,1e-3],[1e-3,1e-3], [1e-3,1e-3], 
                         [2.3e-4, 1.25e-4], [2.3e-4, 1.25e-4], [2.3e-4, 1.25e-4],
                         [2.3e-4, 1.25e-4], [2.3e-4, 1.25e-4],
                         [1e-3,1e-3], [1e-3,1e-3], [1e-3,1e-3]])
-    phase = np.radians(51)
+    phase = np.radians(51.4)
     thicknesses=np.array([d1, d2, d3,sp_thick,
                         sp_thick, sp_thick, sp_thick, 
                         sp_thick, d3, d2, d1])
@@ -187,11 +309,77 @@ def penalty5(x):
         penalty[i] += np.sum(np.square(B[i]-ideal_muell_mat))
     return np.sum(penalty)
 
+def grad_penalty5(x):
+    grad = np.zeros(x.size)
+    for i in range(grad.size):
+        xp=np.copy(x)
+        xm=np.copy(x)
+        xp[i]+=1e-6
+        xm[i]-=1e-6
+        grad[i]=(penalty5(xp)-penalty5(xm))/2e-6
+    return grad
 
-print('Optimization for the 5-stack AHWP with 3 AR layers')
-x3 = [1.5, 2, 2.5, .3,.3,.3]
-res5 = minimize(penalty5, x3, method='BFGS', jac=grad_penalty3, options={'disp':True})
-print('Indices, Thicknesses(mm)', res5.x)
+def big_penalty(x):
+    n1=x[0]; n2=x[1]; n3=x[2]; d1=x[3]; d2=x[4]; d3=x[5]; 
+    s1=x[6];s2=x[7];s3=x[8]; a1=x[9]; a2=x[10]; a3=x[11];
+    sp_index = np.array([3.019, 3.336])
+    angles = np.array([0.,0.,0., a1,a2,a3, 0.,0.,0.])*np.pi/180.0
+    losses = np.array([ [1e-3,1e-3],[1e-3,1e-3], [1e-3,1e-3], 
+                        [2.3e-4, 1.25e-4], [2.3e-4, 1.25e-4], [2.3e-4, 1.25e-4],
+                        [1e-3,1e-3], [1e-3,1e-3], [1e-3,1e-3]])
+    phase = np.radians(57.1)
+    thicknesses=np.array([d1, d2, d3, s1, s2, s3, d3, d2, d1])
+    indices=np.array([[n1, n1],[n2, n2],[n3, n3], 
+                    sp_index, sp_index, sp_index,
+                      [n3,n3],[n2,n2], [n1,n1]])
+    nu = np.array((80,85,90,95,100,105,110,135,140,145,150,155,160,165))
+
+    B = np.zeros((nu.size, 4,4))
+    penalty = np.zeros(nu.size)
+    ideal_muell_mat = np.array(((1,0,0,0),(0,1,0,0),(0,0,-1,0),(0,0,0,-1)))
+    phase_rot = np.array(((1.,0.,0.,0.), (0.,np.cos(-4*phase),np.sin(-4*phase),0.),
+                    (0.,-np.sin(-4*phase),np.cos(-4*phase),0.),(0.,0.,0.,1)))
+    for i, freq in enumerate(nu):
+
+        dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=0.0)
+        dumbeam.set_hwp_mueller(thicknesses=thicknesses, 
+                        indices=indices, 
+                        losses=losses, angles=angles)
+        B[i]=np.dot(phase_rot,dumbeam.hwp_mueller)
+
+        penalty[i] = np.sum(np.square(B[i]-ideal_muell_mat))
+        dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=10.0)
+        dumbeam.set_hwp_mueller(thicknesses=thicknesses, 
+                        indices=indices, 
+                        losses=losses, angles=angles)
+        B[i]=np.dot(phase_rot,dumbeam.hwp_mueller)
+        penalty[i] += np.sum(np.square(B[i]-ideal_muell_mat))
+    return np.sum(penalty)
+
+def big_gradient(x):
+    grad = np.zeros(x.size)
+    for i in range(grad.size):
+        xp=np.copy(x)
+        xm=np.copy(x)
+        xp[i]+=1e-6
+        xm[i]-=1e-6
+        grad[i]=(big_penalty(xp)-big_penalty(xm))/2e-6
+    return grad
+
+print('Optimization for the 1-stack HWP with 1 AR layer')
+x_mono1 = [1.1,0.1,3.]
+res_mono1 = minimize(penalty_mono1, x_mono1, method='BFGS', jac=grad_penalty_mono1, options={'disp':True})
+print('Indices, Thicknesses(mm)', res_mono1.x)
+
+print('Optimization for the 1-stack HWP with 2 AR layers')
+x_mono2 = [1.1, 2., 0.4, 0.3, 3.78]
+res_mono2 = minimize(penalty_mono2, x_mono2, method='BFGS', jac=grad_penalty_mono2, options={'disp':True})
+print('Indices, Thicknesses(mm)', res_mono2.x)
+
+print('Optimization for the 1-stack HWP with 3 AR layers')
+x_mono = [1.1, 2., 2.6, 0.4, 0.3, 0.2, 3.78]
+res_mono = minimize(penalty_monoref, x_mono, method='BFGS', jac=grad_penalty_monoref, options={'disp':True})
+print('Indices, Thicknesses(mm)', res_mono.x)
 
 print('Optimization for the 3-stack AHWP with 1 AR layer')
 x1 = [2.2, 0.4]
@@ -208,7 +396,10 @@ x3 = [1.5, 2, 2.5, .3,.3,.3]
 res3 = minimize(penalty3, x3, method='BFGS', jac=grad_penalty3, options={'disp':True})
 print('Indices, Thicknesses(mm)', res3.x)
 
-
+print('Optimization for the 5-stack AHWP with 3 AR layers')
+x3 = [1.5, 2, 2.5, .3,.3,.3]
+res5 = minimize(penalty5, x3, method='BFGS', jac=grad_penalty5, options={'disp':True})
+print('Indices, Thicknesses(mm)', res5.x)
 
 
 
