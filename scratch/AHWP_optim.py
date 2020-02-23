c624c603ae05e6337737b45dadbc1be815d6049a
AERAdler <alexandre.adler@mapadler.ch>
2020-02-23 11:59:50 +0100

    computing penalty function, phase through penalty function

--- scratch/AHWP_optim.py
+++ scratch/AHWP_optim.py
@@ -373,6 +373,85 @@ def big_gradient(x):
         grad[i]=(big_penalty(xp)-big_penalty(xm))/2e-6
     return grad
 
+def quick_penalty(model_name, phase95=0., phase150=0.):
+    penalty = 0.
+    nu = np.array((80,85,90,95,100,105,110,135,140,145,150,155,160,165))
+    ideal_muell_mat = np.array(((1,0,0,0),(0,1,0,0),(0,0,-1,0),(0,0,0,-1)))
+
+    for i, freq in enumerate(nu):
+
+        if (freq<130):
+            phase = phase95
+        elif (freq>130):
+            phase = phase150
+        phase_rot = np.array(((1.,0.,0.,0.), (0.,np.cos(-4*phase),np.sin(-4*phase),0.),
+                    (0.,-np.sin(-4*phase),np.cos(-4*phase),0.),(0.,0.,0.,1)))
+        dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=0.0)
+        dumbeam.set_hwp_mueller(model_name=model_name)
+        B=np.dot(phase_rot,dumbeam.hwp_mueller)
+        penalty += np.sum(np.square(B-ideal_muell_mat))*modBB(freq, 1.59)
+        dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=10.0)
+        dumbeam.set_hwp_mueller(model_name=model_name)
+        B=np.dot(phase_rot,dumbeam.hwp_mueller)
+        penalty += np.sum(np.square(B-ideal_muell_mat))*modBB(freq, 1.59)
+    return .5*np.sum(penalty)
+
+def penalty95(model_name, phase95=0.):
+    penalty = 0.
+    nu = np.array((80,85,90,95,100,105,110))
+    ideal_muell_mat = np.array(((1,0,0,0),(0,1,0,0),(0,0,-1,0),(0,0,0,-1)))
+    phase = phase95
+    for i, freq in enumerate(nu):        
+        phase_rot = np.array(((1.,0.,0.,0.), (0.,np.cos(-4*phase),np.sin(-4*phase),0.),
+                    (0.,-np.sin(-4*phase),np.cos(-4*phase),0.),(0.,0.,0.,1)))
+        dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=0.0)
+        dumbeam.set_hwp_mueller(model_name=model_name)
+        B=np.dot(phase_rot,dumbeam.hwp_mueller)
+
+        penalty += np.sum(np.square(B-ideal_muell_mat))#*modBB(freq, 1.59)
+        dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=10.0)
+        dumbeam.set_hwp_mueller(model_name=model_name)
+        B=np.dot(phase_rot,dumbeam.hwp_mueller)
+        penalty += np.sum(np.square(B-ideal_muell_mat))#*modBB(freq, 1.59)
+    return .5*np.sum(penalty)
+
+def penalty150(model_name, phase150=0.):
+    penalty = 0.
+    nu = np.array((135,140,145,150,155,160,165))
+    ideal_muell_mat = np.array(((1,0,0,0),(0,1,0,0),(0,0,-1,0),(0,0,0,-1)))
+    phase = phase150
+    for i, freq in enumerate(nu):        
+        phase_rot = np.array(((1.,0.,0.,0.), (0.,np.cos(-4*phase),np.sin(-4*phase),0.),
+                    (0.,-np.sin(-4*phase),np.cos(-4*phase),0.),(0.,0.,0.,1)))
+        dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=0.0)
+        dumbeam.set_hwp_mueller(model_name=model_name)
+        B=np.dot(phase_rot,dumbeam.hwp_mueller)
+
+        penalty += np.sum(np.square(B-ideal_muell_mat))#*modBB(freq, 1.59)
+        dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=10.0)
+        dumbeam.set_hwp_mueller(model_name=model_name)
+        B=np.dot(phase_rot,dumbeam.hwp_mueller)
+        penalty += np.sum(np.square(B-ideal_muell_mat))#*modBB(freq, 1.59)
+    return .5*np.sum(penalty)
+
+def phase_through_penalty(model_name):
+    phases = np.linspace(50,60, num=101, endpoint=True)
+    best_phase9 = 0
+    best_phase150 = 0
+    bestpenalty95 = 100
+    bestpenalty150 = 100
+    for i, phi in enumerate(phases):
+        if (penalty95(model_name, phi)<bestpenalty95):
+            bestpenalty95 = penalty95(model_name, phi)
+            best_phase95 = phi
+
+        if (penalty150(model_name, phi)<bestpenalty150):
+            bestpenalty150 = penalty150(model_name, phi)
+            best_phase150 = phi
+    print('95GHz phase: %.1f, 150GHz phase: %.1f, combined penalty score: %f'%
+        (best_phase95, best_phase150, bestpenalty95+bestpenalty150))
+    return  best_phase95, best_phase150, bestpenalty95+bestpenalty150
+
 # print('Optimization for the 1-stack HWP with 1 AR layer')
 # x_mono1 = [1.1,0.1,3.]
 # res_mono1 = minimize(penalty_mono1, x_mono1, method='BFGS', jac=grad_penalty_mono1, options={'disp':True})
@@ -383,11 +462,11 @@ def big_gradient(x):
 # res_mono2 = minimize(penalty_mono2, x_mono2, method='BFGS', jac=grad_penalty_mono2, options={'disp':True})
 # print('Indices, Thicknesses(mm)', res_mono2.x)
 
-print('Optimization for the 1-stack HWP with 3 AR layers')
-x_mono = [1.1, 2., 2.6, 0.4, 0.3, 0.2, 3.78]
-res_mono = minimize(penalty_monoref, x_mono, method='L-BFGS-B', jac=grad_penalty_monoref, options={'disp':True},
-    bounds=[(1., 3.1), (1., 3.1), (1., 3.1), (1e-3, 5), (1e-3, 5), (1e-3, 5), (1e-3, 5)])
-print('Indices, Thicknesses(mm), angle (°)', res_mono.x)
+# print('Optimization for the 1-stack HWP with 3 AR layers')
+# x_mono = [1.1, 2., 2.6, 0.4, 0.3, 0.2, 3.78]
+# res_mono = minimize(penalty_monoref, x_mono, method='L-BFGS-B', jac=grad_penalty_monoref, options={'disp':True},
+#     bounds=[(1., 3.1), (1., 3.1), (1., 3.1), (1e-3, 5), (1e-3, 5), (1e-3, 5), (1e-3, 5)])
+# print('Indices, Thicknesses(mm), angle (°)', res_mono.x)
 
 # print('Optimization for the 3-stack AHWP with 1 AR layer')
 # x1 = [2.2, 0.4]
@@ -399,91 +478,29 @@ print('Indices, Thicknesses(mm), angle (°)', res_mono.x)
 # res2 = minimize(penalty2, x2, method='BFGS', jac=grad_penalty2, options={'disp':True})
 # print('Indices, Thicknesses(mm)', res2.x)
 
-print('Optimization for the 3-stack AHWP with 3 AR layer')
-x3 = [1.5, 2, 2.5, .3, .3, .3, 3.8, 50]
-res3 = minimize(penalty3, x3, method='L-BFGS-B', jac=grad_penalty3, options={'disp':True},
-    bounds=[(1., 3.1), (1., 3.1), (1., 3.1), (1e-3, 5), (1e-3, 5), (1e-3, 5), (1e-3, 5), (0,90)])
-print('Indices, Thicknesses(mm), angle (°)', res3.x)
+# print('Optimization for the 3-stack AHWP with 3 AR layer')
+# x3 = [1.5, 2, 2.5, .3, .3, .3, 3.8, 50]
+# res3 = minimize(penalty3, x3, method='L-BFGS-B', jac=grad_penalty3, options={'disp':True},
+#     bounds=[(1., 3.1), (1., 3.1), (1., 3.1), (1e-3, 5), (1e-3, 5), (1e-3, 5), (1e-3, 5), (0,90)])
+# print('Indices, Thicknesses(mm), angle (°)', res3.x)
 
-print('Optimization for the 5-stack AHWP with 3 AR layers')
-x3 = [1.5, 2, 2.5, .3, .3, .3, 3.8, 50]
-res5 = minimize(penalty5, x3, method='L-BFGS-B', jac=grad_penalty5, options={'disp':True}, 
-    bounds=[(1., 3.1), (1., 3.1), (1., 3.1), (1e-3, 5), (1e-3, 5), (1e-3, 5), (1e-3, 5), (0,90)])
-print('Indices, Thicknesses(mm)', res5.x)
+# print('Optimization for the 5-stack AHWP with 3 AR layers')
+# x3 = [1.5, 2, 2.5, .3, .3, .3, 3.8, 50]
+# res5 = minimize(penalty5, x3, method='L-BFGS-B', jac=grad_penalty5, options={'disp':True}, 
+#     bounds=[(1., 3.1), (1., 3.1), (1., 3.1), (1e-3, 5), (1e-3, 5), (1e-3, 5), (1e-3, 5), (0,90)])
+# print('Indices, Thicknesses(mm)', res5.x)
 
+print('Current penalty 3BR old, CMB: %f\n Current penalty 3BR new, CMB: %f\n Current penalty 5BR old, CMB: %f\n Current penalty 5BR new, CMB: %f\n' %
+    (quick_penalty('3AR3BRcent', phase95=57.1, phase150=57.6), quick_penalty('3AR3BR_new', phase95=59.2, phase150=57.4) ,
+    quick_penalty('3AR5BR', phase95=51.3, phase150=51.3) , quick_penalty('3AR5BR_new', phase95=53.6, phase150=53.0)))
 
-
-# nu_0 = (95+150)/2.
-# epsilon = 150/nu_0 - 1.
-# delta = np.pi/3 - (epsilon*np.pi/2)**2/(2*np.sqrt(3))
-
-# nu_1 = (95**2+150**2)/(95+150)
-# epsilon_1 = 165 /nu_1 - 1.
-# delta_1 = np.pi/3 - (epsilon_1*np.pi/2)**2/(2*np.sqrt(3))
-
-# sp_index = np.array([3.019, 3.336])
-# sp_thick = 3.86
-# angles = np.array([0.,0.,52.5,0.,0.])*np.pi/180.0
-# losses = np.array([[1e-3,1e-3], [2.3e-4, 1.25e-4], [2.3e-4, 1.25e-4], [2.3e-4, 1.25e-4],[1e-3,1e-3 ]])
-# phase = np.radians(57.1)
-
-# ar_thick = np.linspace(.2, .45, 51)
-# ar_indx = np.linspace(1.4, 2.2, 17)
-
-# phase_rot = np.array(((1.,0.,0.,0.), (0.,np.cos(-4*phase),np.sin(-4*phase),0.),
-#                     (0.,-np.sin(-4*phase),np.cos(-4*phase),0.),(0.,0.,0.,1))) #Rotate back by twice the phase
-
-# nu = np.array((80, 85,90,95,100,105,110,135,140,145,150,155,160,165))
-# theta = np.linspace(0,180,1801)
-
-# ideal_muell_mat = np.array(((1,0,0,0),(0,1,0,0),(0,0,-1,0),(0,0,0,-1)))
-
-# F_array = np.zeros((ar_indx.size,ar_thick.size)) 
+# print('3BR old, CMB:')
+# phase_through_penalty('3AR3BRcent')
+# print('3BR new, CMB:')
+# phase_through_penalty('3AR3BR_new')
+# print('5BR old, CMB:')
+# phase_through_penalty('3AR5BR')
+# print('5BR new, CMB:')
+# phase_through_penalty('3AR5BR_new')
 
 
-
-# A = np.zeros((nu.size,3,theta.size), dtype=complex)
-
-# for k, arin in enumerate(ar_indx):
-#     print(arin)
-#     for j, arth in enumerate(ar_thick):
-#         B = np.zeros((nu.size, 4,4))
-#         penalty = np.zeros(nu.size)
-#         for i, freq in enumerate(nu):
-               
-#             # print('Frequency:', freq)
-#             # dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=0.0)
-#             # dumbeam.set_hwp_mueller(model_name='1AR3BRcent')
-#             # A[i,:,:] = dumbeam.get_mueller_top_row_full(psi=np.zeros(theta.size), xi=np.zeros(theta.size), theta=np.radians(theta))
-#             #np.radians(theta[np.argmax(np.real(A[i,1,:900]), axis=0)])
-
-#             dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=0.0)
-#             dumbeam.set_hwp_mueller(thicknesses=np.array([arth, sp_thick, sp_thick, sp_thick, arth]), 
-#                             indices=np.array([[arin, arin], sp_index, sp_index, sp_index, [arin,arin]]), 
-#                             losses=losses, angles=angles)
-#             B[i]=np.dot(phase_rot,dumbeam.hwp_mueller)
-#             if (arin==1.8 and arth==0.35):
-#                 print('Frequency: ', freq)
-#                 print('Unrotated Mueller:\n', dumbeam.hwp_mueller)
-#                 print('Rotated Mueller:\n', B[i])
-#             penalty[i] = np.sum(np.square(B[i]-ideal_muell_mat))
-#             dumbeam = Beam(name='testbeam', sensitive_freq=freq, el=10.0)
-#             dumbeam.set_hwp_mueller(thicknesses=np.array([arth, sp_thick, sp_thick, sp_thick, arth]), 
-#                             indices=np.array([[arin, arin], sp_index, sp_index, sp_index, [arin,arin]]), 
-#                             losses=losses, angles=angles)
-#             B[i]=np.dot(phase_rot,dumbeam.hwp_mueller)
-#             penalty[i] += np.sum(np.square(B[i]-ideal_muell_mat))
-
-#         F_array[k,j] = np.sum(penalty)
-#     # print('HWP Mueller is:\n', dumbeam.hwp_mueller)
-#     # print('Phase is:\n',np.degrees(phase))
-#     # print('Rotated Mueller is:\n', np.dot(phase_rot,dumbeam.hwp_mueller))
-#     # print('Penalty is: \n',penalty[i])#Sum over all elements of squared differences
-# # print('Total_penalty:\n', np.sum(penalty))
-
-# cont_f =plt.contourf(ar_thick, ar_indx, F_array, levels=30)
-# plt.colorbar(cont_f)
-# plt.xlabel('AR thickness (mm)')
-# plt.ylabel('AR refraction index')
-# plt.title('Penalty function for a 3 layer AHWP with 1AR layer, phase corrected at 95 and 150GHz')
-# plt.show()
